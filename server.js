47a66d182b8c75ffb852ad49ab08ee2a703a3971
malurmaia <marialuisaribeiromaia@gmail.com>
2022-11-28 17:50:46 -0300

    conecta banco com front

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const userRoutes = require('./Routes/user.routes');
 const productRoutes = require('./Routes/product.routes');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res)=>{
     res.end("")
 });
 
 app.use("/user-api", userRoutes);
 app.use("/product", productRoutes );
-app.listen(3000,()=>console.log("O servidor está rolando"));
+app.listen(3001,()=>console.log("O servidor está rolando"));
